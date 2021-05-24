1c2ef8b305c9d400bbc925f8a9036bc7a1c7a77d
FlutLab.io <>
2021-05-24 23:05:56 +0000

    Autosave

--- lib/pages/first_pages.dart
+++ lib/pages/first_pages.dart
@@ -4,17 +4,22 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 120.0,
+      width: 120.0,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+              'assets/images/burger.jpg'),
+          fit: BoxFit.fill,
+        ),
+        shape: BoxShape.circle,
+      ),
       child: Center(
         child: Text(
           'Second Page',
           style: new TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold, color: Colors.black),
         ), //Text
-        decoration: new BoxDecoration(
-          image: new DecorationImage(
-              image: new AssetImage("assets/images/api.png"),
-              fit: BoxFit.fill,
-          ),
-        ),
+        
       ), // center
     ); //container
   } // widget
