b477c39841a9b52d631454441dd35a35cc2684e2
FlutLab.io <>
2021-05-24 22:32:27 +0000

    Autosave

--- lib/pages/first_pages.dart
+++ lib/pages/first_pages.dart
@@ -3,16 +3,16 @@ import 'package:flutter/material.dart';
 class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        return DecoratedBox(
+  // ...
+  return DecoratedBox(
     decoration: BoxDecoration(
       image: DecorationImage(
         image: AssetImage('assets/images/api.png'),
+        // ...
       ),
+      // ...
     ),
-  ),
-      ), // center
-    ); //container
-  } // widget
+  );
+  // ...
+}
 } // first page
