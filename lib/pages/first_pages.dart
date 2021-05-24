965ad627e89e772c4ea2bf0dd9a1525a72ea3d7f
FlutLab.io <>
2021-05-24 23:06:42 +0000

    Autosave

--- lib/pages/first_pages.dart
+++ lib/pages/first_pages.dart
@@ -4,12 +4,10 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 120.0,
-      width: 120.0,
       decoration: BoxDecoration(
         image: DecorationImage(
           image: AssetImage(
-              'assets/images/burger.jpg'),
+              ),
           fit: BoxFit.fill,
         ),
         shape: BoxShape.circle,
