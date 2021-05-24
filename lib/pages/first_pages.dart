60ed22b61fd02f55f9db10a2d7d46a517cc24e59
FlutLab.io <>
2021-05-24 22:56:14 +0000

    Autosave

--- lib/pages/first_pages.dart
+++ lib/pages/first_pages.dart
@@ -3,11 +3,26 @@ import 'package:flutter/material.dart';
 class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-  body: Card(
-       child: Column(
-     children: <Widget>[
-       Image.asset('assets/images/api.png'),
-     ],
-   ));
+  body: Center(
+    child: Stack(
+      children: <Widget>[
+        Container(
+          alignment: Alignment.center,
+          child: Image.network(
+           'https://raw.githubusercontent.com/Danaromero/Mis_Imagenes_Romero/main/monitor.png'            height: 250,
+            
+          ),
+        ),
+        Container(
+            alignment: Alignment.center,
+            child: Text(
+              'Text Message',
+              style: TextStyle(color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22.0),
+            )),
+      ],
+    ),
+  );
 }
 } // first page
