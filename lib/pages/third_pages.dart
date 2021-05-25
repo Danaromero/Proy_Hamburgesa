0fd230450a06efa12fb6cd4ef5fa7ef905d6c3f6
FlutLab.io <>
2021-05-25 22:25:12 +0000

    Autosave

--- lib/pages/third_pages.dart
+++ lib/pages/third_pages.dart
@@ -33,7 +33,13 @@ class _ThirdPageState extends State<ThirdPage> {
             SizedBox(
               height: 20.0,
             ),
-            
+            Container(
+              height: 200,
+              width: 350,
+              decoration: BoxDecoration(
+                color: Color(0xFFEEEEEE)
+              ),
+            ),//Fin COntainer
           ], //Widget
         ), //Column
       ), //ConstrainedBox
