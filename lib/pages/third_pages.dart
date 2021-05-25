853a1c40c3b6721ff76ee11e388c0fa20c3a52f2
FlutLab.io <>
2021-05-25 20:52:00 +0000

    Autosave

--- lib/pages/third_pages.dart
+++ lib/pages/third_pages.dart
@@ -1,44 +1,44 @@
 import 'package:flutter/material.dart';
 
-class ThirdPage extends StatefulWidget{
+class ThirdPage extends StatefulWidget {
   @override
   _ThirdPageState createState() => _ThirdPageState();
 }
 
+const List<Color> orangeGradients = [
+  Color(0xFFE3F2FD),
+  Color(0xFFE3F2FD),
+  Color(0xFFE3F2FD),
+];
 
+const List<Color> greenGradients = [
+  Color(0xFFBBDEFB),
+  Color(0xFFBBDEFB),
+  Color(0xFFBBDEFB),
+];
+
+const List<Color> blueGradients = [
+  Color(0xFF90CAF9),
+  Color(0xFF90CAF9),
+  Color(0xFF90CAF9),
+];
 
 class _ThirdPageState extends State<ThirdPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Dana Romero Widgets App',
-          style: TextStyle(color: Colors.white),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.red[200],
-      ),
-      backgroundColor: Colors.white,
-      body: Container(
-        child: Center(
-          child: Column(
-            // center the children
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Container(
-                child: new Image.asset(
-                   'assets/images/prom.jpg',
-                ),
-              ),
-              Text(
-                "Soy la tercer pagina!",
-                style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 30),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+    return Material(
+      child: ConstrainedBox(constraints: BoxConstraints(maxWidth: 300, minWidth: 200),
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 80.0, ),
+          Container(
+            color: Colors.pink,
+            height: 200,
+            width: 350,
+          ),//Container
+        ],//Widget
+      ),//Column
+      ),//ConstrainedBox
+    );//Material
   } // widget
 } // first page
