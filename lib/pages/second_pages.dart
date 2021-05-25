ad21fa91647d6536b47ca55486185bd50a540aca
FlutLab.io <>
2021-05-25 20:11:33 +0000

    Autosave

--- lib/pages/second_pages.dart
+++ lib/pages/second_pages.dart
@@ -13,62 +13,67 @@ class SecondPage extends StatelessWidget {
           height: double.infinity,
           padding: EdgeInsets.symmetric(horizontal: 20.0),
           child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 50,
-              ),
-              Container(
-                child: Text(
-                  'Burger King',
-                  style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 45.0, color: Colors.red),
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 50,
                 ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Column(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(2.0),
-                    child: Stack(
-                      children: <Widget>[
-                        Align(
-                          child: new Image(
-                            width: 300.0,
-                            height: 200.0,
-                            image: new AssetImage('assets/images/burger.jpg'),
-                          ), //image
-                        ), //Align
-                      ], //Widget[]
-                    ), //Stack
-                  ), //Container
-                  SizedBox(height: 40,),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: "Email",
-                      icon: Icon(Icons.email)
-                    ), ),
-                  SizedBox(height: 40,),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: "Paswoord",
-                      icon: Icon(Icons.vpn_key)
-                    ), ),
-                  SizedBox(height: 40,),
-                  
-                        FlatButton(
-                          child: Text('Cancelar'),
-                          color: Colors.blue[200],
-                          padding: EdgeInsets.all(20.0),
-                          onPressed: () {                           
-                          },
-                        ), //Fin FlatButton
-                ], //Widget
-              ), //Column
-            ], //Widget
-          ), //Column
-          ),//Fin Single
+                Container(
+                  child: Text(
+                    'Burger King',
+                    style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 45.0, color: Colors.red),
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(2.0),
+                      child: Stack(
+                        children: <Widget>[
+                          Align(
+                            child: new Image(
+                              width: 300.0,
+                              height: 200.0,
+                              image: new AssetImage('assets/images/burger.jpg'),
+                            ), //image
+                          ), //Align
+                        ], //Widget[]
+                      ), //Stack
+                    ), //Container
+                    SizedBox(
+                      height: 40,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(hintText: "Email", icon: Icon(Icons.email)),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(hintText: "Paswoord", icon: Icon(Icons.vpn_key)),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    FlatButton(
+                      padding: EdgeInsets.symmetric(horizontal: 30.0),
+                      child: Text('Cancelar'
+                      style: TextStyle(fontSize: 15.0),),
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(30.0),
+                      onPressed: () {},
+                    ), //Fin FlatButton
+                    SizedBox(
+                      height: 40,
+                    ),
+                  ], //Widget
+                ), //Column
+              ], //Widget
+            ), //Column
+          ), //Fin Single
         ), //Container
       ), //SafeArea
     ); //Scaffold
