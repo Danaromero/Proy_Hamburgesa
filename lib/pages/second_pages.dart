071793e8774e8abc1a112413da803ff4eb5cc6b7
FlutLab.io <>
2021-05-25 19:20:50 +0000

    Autosave

--- lib/pages/second_pages.dart
+++ lib/pages/second_pages.dart
@@ -1,37 +1,48 @@
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 
 class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Dana Romero Widgets App',
-          style: TextStyle(color: Colors.white),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.red[200],
-      ),
-      backgroundColor: Colors.white,
-      body: Container(
-        child: Center(
-          child: Column(
-            // center the children
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Container(
-                child: new Image.asset(
-                  'assets/images/calendario.jpg',
-                ),
-              ),
-              Text(
-                "Soy la segunda pagina!",
-                style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 30),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+      resizeToAvoidBottomPadding: false,
+      body: SafeArea(
+      child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 30.0 ),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 100, ),
+            Container(
+             child: Text('Burger King',
+             style: new TextStyle(fontWeight: FontWeight.bold,
+             fontSize: 45.0, color: Colors.red),),
+            ),
+            SizedBox(height: 40, ),
+            Column(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(2.0),
+                  child: Stack(
+                    children: <Widget>[
+                      Align(
+                        child: new Image(
+                          width: 300.0,
+                          height: 200.0,
+                          image: new AssetImage('assets/images/burger.jpg'),
+                        ),//image
+                      ),//Align
+                    ],//Widget[]
+                  ),//Stack
+                ),//Container
+                SizedBox(height: 40, ),
+              ],//Widget
+            ),//Column
+          ],//Widget
+        ), //Column
+      ),//Container
+    ),//SafeArea
+    );//Scaffold
   } // widget
 } // first page
