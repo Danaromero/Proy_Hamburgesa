80d283259cc4e24eb18476c1255f43086a59ba37
FlutLab.io <>
2021-05-25 21:00:19 +0000

    Autosave

--- lib/pages/third_pages.dart
+++ lib/pages/third_pages.dart
@@ -36,6 +36,29 @@ class _ThirdPageState extends State<ThirdPage> {
             height: 200,
             width: 350,
           ),//Container
+          SizedBox(height: 20.0, ),
+          Container(
+            color: Colors.pink,
+            height: 200,
+            width: 350,
+            decoration: ShapeDecoration(shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0)
+            ),//RoundedRectangleBorder
+             gradient: LinearGradient(
+               colors: orangeGradients,
+               begin: Alignment.topLeft,
+               end: Alignment.bottomRight
+             )//LinearGradient
+            ),//ShapeDecoration
+            child: Container(
+              padding: EdgeInsets.only(top: 35.0, bottom: 16),
+              child: Text("Iniciando", style: TextStyle(
+                color: Colors.white,
+                fontSize: 38,
+                fontWeight: FontWeight.bold
+              ),),//TextStyle y TEXT
+            ),//Container
+          ),//Container
         ],//Widget
       ),//Column
       ),//ConstrainedBox
